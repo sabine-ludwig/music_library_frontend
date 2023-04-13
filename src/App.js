53123e081e3de750b34886041548166222058a2c
sabine-ludwig <125092312+sabine-ludwig@users.noreply.github.com>
2023-04-13 09:53:11 -0700

    initial set-up in app.js

--- src/App.js
+++ src/App.js
@@ -1,23 +1,34 @@
-import logo from './logo.svg';
 import './App.css';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 
 function App() {
+
+  const [songs, setSongs] = useState([]);
+
+  useEffect(() => {
+    getAllSongs();
+  }, [])
+
+  async function getAllSongs(){
+    try{
+      let response = await axios.get('http://127.0.0.1:5000/api/songs');
+      setSongs(response.data.songs);
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  async function searchSongs()
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div>
+        {/* navbar */}
+      </div>
+      <div>
+        {/* table */}
+      </div>
     </div>
   );
 }
